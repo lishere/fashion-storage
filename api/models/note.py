6409fc1686acb053eee6b9ccf9445a01b0746685
Laurent Dellere <laurent@tacticaltech.org>
2017-01-06 10:32:39 +0100

    implementing database shema

--- api/models/note.py
+++ api/models/note.py
@@ -1,15 +1,15 @@
+
+# -*- coding: utf-8 -*-
+
 from django.db import models
 #from django.contrib.contenttypes import *
 
-#import api.lists.country_list
-#import api.lists.store_list
-
 class Note(models.Model):
-    created             = models.DateTimeField(auto_now_add=True)
+    created             = models.DateTimeField(auto_now_add=True, blank=False)
     #attached_to         = models.GenericForeignKey('content_type', 'object_id')
-    body                = models.TextField()
+    body                = models.TextField(blank=False)
     # user
 
     class Meta:
-        ordering    = 'created'
+        ordering    = ('created',)
         app_label   = 'api'
--- api/models/store.py
+++ api/models/store.py
@@ -1,32 +1,36 @@
+
+# -*- coding: utf-8 -*-
+
 from django.db import models
 #from django.contrib.contenttypes import *
 
-#import api.lists.country_list
-#import api.lists.store_list
-
-
 
 class Store(models.Model):
-    created             = models.DateTimeField(auto_now_add=True)
-    is_active           = models.BooleanField(default=True)
-    is_online_shop      = models.BooleanField(default=False)
-    name                = models.CharField(max_length=250)
-    address             = models.CharField(max_length=125)
-    zip_code            = models.CharField(max_length=50)
-    city                = models.CharField(max_length=50)
+
+    SHIPPING_AGREMENTS = (('store_pays', 'Store pays shippings costs'),('split', 'Shipping costs are split'),('we_pay', 'We pay shipping costs'),)
+    PARTNER_TYPES = (('commission','Commission'),('direct_buyer','Direct buyer'),('mixed','Mixed'),)
+
+    created             = models.DateTimeField(auto_now_add=True, blank=False)
+    updated             = models.DateTimeField(auto_now_add=True, blank=True)
+    is_active           = models.BooleanField(default=True, blank=True)
+    is_online_shop      = models.BooleanField(default=False, blank=True)
+    name                = models.CharField(max_length=250, blank=False)
+    address             = models.CharField(max_length=125, blank=True)
+    zip_code            = models.CharField(max_length=50, blank=True)
+    city                = models.CharField(max_length=50, blank=False)
     #country             = models.ForeignKey(
     #                        Country
     #                      )
-    test1            = models.CharField(max_length=50)
+
     # contact_person
     # shipping_address
     # invoice_address
-    #shipping_agrement   = models.CharField(max_length=20, choices=api.lists.store_list.SHIPPING_AGREMENTS)
-    #partner_type        = models.CharField(max_length=20, choices=api.lists.store_list.PARTNER_TYPES)
-    commission_rate     = models.IntegerField(default=50)
+    shipping_agrement   = models.CharField(max_length=20, choices=SHIPPING_AGREMENTS, blank=True)
+    partner_type        = models.CharField(max_length=20, choices=PARTNER_TYPES, blank=True)
+    commission_rate     = models.IntegerField(default=50, blank=True)
     website             = models.URLField(blank=True)
     facebook            = models.URLField(blank=True)
 
     class Meta:
-        ordering    = 'name'
+        ordering    = ('name',)
         app_label   = 'api'
