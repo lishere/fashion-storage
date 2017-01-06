b7e2871473b80c07eab2b87db193a73ef4e19f21
Laurent Dellere <laurent@tacticaltech.org>
2017-01-06 13:11:47 +0100

    registering models in admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models.country import Country
+from .models.store import Store
+from .models.note import Note
+
+admin.site.register(Country)
+admin.site.register(Store)
+admin.site.register(Note)
