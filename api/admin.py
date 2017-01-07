03414dd6f11bd916395e70aaebb35a179bf17808
Laurent Dellere <laurent@tacticaltech.org>
2017-01-07 09:21:00 +0100

    adding shipping address to admin forms

--- api/admin.py
+++ api/admin.py
@@ -7,6 +7,7 @@ from .models.country import Country
 from .models.store import Store
 from .models.note import Note
 from .models.invoice_address import Invoice_address
+from .models.shipping_address import Shipping_address
 from .models.person import Person
 from .models.customer import Customer
 
@@ -26,6 +27,12 @@ class InvoiceAddressAdmin(admin.ModelAdmin):
         NoteInline,
     ]
 
+class ShippingAddressAdmin(admin.ModelAdmin):
+    inlines = [
+        NoteInline,
+    ]
+
+
 class PersonAdmin(admin.ModelAdmin):
     inlines = [
         NoteInline,
@@ -41,5 +48,6 @@ admin.site.register(Country)
 admin.site.register(Store, StoreAdmin)
 admin.site.register(Note)
 admin.site.register(Invoice_address, InvoiceAddressAdmin)
+admin.site.register(Shipping_address, ShippingAddressAdmin)
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Customer, CustomerAdmin)
