90a26e0d63ea45022dcd948817c3b78d8cd4ff77
Laurent Dellere <laurent@tacticaltech.org>
2017-01-07 08:40:07 +0100

    registering models

--- api/admin.py
+++ api/admin.py
@@ -7,6 +7,8 @@ from .models.country import Country
 from .models.store import Store
 from .models.note import Note
 from .models.invoice_address import Invoice_address
+from .models.person import Person
+from .models.customer import Customer
 
 class NoteInline(GenericTabularInline):
     model = Note
@@ -20,3 +22,5 @@ admin.site.register(Country)
 admin.site.register(Store, StoreAdmin)
 admin.site.register(Note)
 admin.site.register(Invoice_address)
+admin.site.register(Person)
+admin.site.register(Customer)
--- api/models/__init__.py
+++ api/models/__init__.py
@@ -2,3 +2,5 @@ from country import Country
 from note import Note
 from invoice_address import Invoice_address
 from store import Store
+from person import Person
+from customer import Customer
