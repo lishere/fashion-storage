34b9c2b25813f2b4242113ff102872a23fe765a6
Laurent Dellere <laurent@tacticaltech.org>
2017-01-09 12:11:57 +0100

    making product variants fields multilingual, migrating

--- api/models/product_variant.py
+++ api/models/product_variant.py
@@ -4,6 +4,7 @@ from django.db import models
 from rest_framework import serializers
 
 from api.models.image import Image
+from api.models.product import Product
 
 
 class Product_variant(models.Model):
@@ -15,7 +16,7 @@ class Product_variant(models.Model):
     is_active                       = models.BooleanField(default=True, blank=True)
     is_organic                      = models.BooleanField(default=False, blank=False, verbose_name="Organic")
 
-    # product
+    product                         = 
     images                          = models.ManyToManyField(Image, blank=True)
 
     rec_gross_sale_price_default    = models.DecimalField(default=0, max_digits=6, decimal_places=2, blank=True, verbose_name='Recommended default gross sale price')
@@ -32,18 +33,36 @@ class Product_variant(models.Model):
     care_instructions_en            = models.TextField(max_length=250, blank=True, verbose_name='English care instructions')
     care_instructions_it            = models.TextField(max_length=250, blank=True, verbose_name='Italian care instructions')
 
-    color_1                         = models.CharField(max_length=50, blank=False, verbose_name='Main color')
-    color_2                         = models.CharField(max_length=50, blank=True, verbose_name='2nd color')
-    color_3                         = models.CharField(max_length=50, blank=True, verbose_name='3rd color')
-    color_4                         = models.CharField(max_length=50, blank=True, verbose_name='4th color')
-    fabric_1                        = models.CharField(max_length=50, blank=False, verbose_name='Main fabric')
-    fabric_2                        = models.CharField(max_length=50, blank=True, verbose_name='2nd fabric')
-    fabric_3                        = models.CharField(max_length=50, blank=True, verbose_name='3rd fabric')
-    fabric_4                        = models.CharField(max_length=50, blank=True, verbose_name='4th fabric')
+    color_1_de                      = models.CharField(max_length=50, blank=False, verbose_name='Main color in German')
+    color_1_en                      = models.CharField(max_length=50, blank=False, verbose_name='Main color in Englisch')
+    color_1_it                      = models.CharField(max_length=50, blank=False, verbose_name='Main color in Italian')
+    color_2_de                      = models.CharField(max_length=50, blank=True, verbose_name='2nd color in German')
+    color_2_en                      = models.CharField(max_length=50, blank=True, verbose_name='2nd color in Englisch')
+    color_2_it                      = models.CharField(max_length=50, blank=True, verbose_name='2nd color in Italian')
+    color_3_de                      = models.CharField(max_length=50, blank=True, verbose_name='3rd color in German')
+    color_3_en                      = models.CharField(max_length=50, blank=True, verbose_name='3rd color in Englisch')
+    color_3_it                      = models.CharField(max_length=50, blank=True, verbose_name='3rd color in Italian')
+    color_4_de                      = models.CharField(max_length=50, blank=True, verbose_name='4th color in German')
+    color_4_en                      = models.CharField(max_length=50, blank=True, verbose_name='4th color in Englisch')
+    color_4_it                      = models.CharField(max_length=50, blank=True, verbose_name='4th color in Italian')
+
+    fabric_1_de                     = models.CharField(max_length=50, blank=False, verbose_name='Main fabric in German')
+    fabric_1_en                     = models.CharField(max_length=50, blank=False, verbose_name='Main fabric in Enlish')
+    fabric_1_it                     = models.CharField(max_length=50, blank=False, verbose_name='Main fabric in Italian')
+    fabric_2_de                     = models.CharField(max_length=50, blank=True, verbose_name='2nd fabric in German')
+    fabric_2_en                     = models.CharField(max_length=50, blank=True, verbose_name='2nd fabric in Englisch')
+    fabric_2_it                     = models.CharField(max_length=50, blank=True, verbose_name='2nd fabric in Italian')
+    fabric_3_de                     = models.CharField(max_length=50, blank=True, verbose_name='3nd fabric in German')
+    fabric_3_en                     = models.CharField(max_length=50, blank=True, verbose_name='3nd fabric in Englisch')
+    fabric_3_it                     = models.CharField(max_length=50, blank=True, verbose_name='3nd fabric in Italian')
+    fabric_4_de                     = models.CharField(max_length=50, blank=True, verbose_name='4nd fabric in German')
+    fabric_4_en                     = models.CharField(max_length=50, blank=True, verbose_name='4nd fabric in Englisch')
+    fabric_4_it                     = models.CharField(max_length=50, blank=True, verbose_name='4nd fabric in Italian')
     fabric_1_percentage             = models.IntegerField(default=100, blank=True, verbose_name='Percentage main fabric', help_text='Percentage of the main fabric in %')
     fabric_2_percentage             = models.IntegerField(default=0, blank=True, verbose_name='Percentage 2nd fabric', help_text='Percentage of 2nd fabric in %')
     fabric_3_percentage             = models.IntegerField(default=0, blank=True, verbose_name='Percentage 3rd fabric', help_text='Percentage of 3rd fabric in %')
     fabric_4_percentage             = models.IntegerField(default=0, blank=True, verbose_name='Percentage 4th fabric', help_text='Percentage of 4th fabric in %')
+
     weight                          = models.SmallIntegerField(default=0, blank=True, help_text='Product weight in grams')
     surface_weight                  = models.SmallIntegerField(default=0, blank=True, help_text='Product surface weight in grams/m²')
 
@@ -75,8 +94,12 @@ class Product_variant_serializer(serializers.HyperlinkedModelSerializer):
                   'care_instructions_de', 'care_instructions_en', 'care_instructions_it',
                   'rec_gross_sale_price_default',
                   'size',
-                  'color_1','color_2','color_3','color_4',
-                  'fabric_1','fabric_2','fabric_3','fabric_4',
+                  'color_1_de','color_2_de','color_3_de','color_4_de',
+                  'color_1_en','color_2_en','color_3_en','color_4_en',
+                  'color_1_it','color_2_it','color_3_it','color_4_it',
+                  'fabric_1_de','fabric_2_de','fabric_3_de','fabric_4_de',
+                  'fabric_1_en','fabric_2_en','fabric_3_en','fabric_4_en',
+                  'fabric_1_it','fabric_2_it','fabric_3_it','fabric_4_it',
                   'fabric_1_percentage','fabric_2_percentage','fabric_3_percentage','fabric_4_percentage',
                   'weight', 'surface_weight'
                  )
